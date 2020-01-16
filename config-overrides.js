4caeb6a6b0d47eb143c00051031501fd90fcae2f
Leon Feng <523684+leon0707@users.noreply.github.com>
2020-01-16 11:02:56 -0500

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -4,8 +4,7 @@ module.exports = function override(config, env) {
   //do stuff with the webpack config...
   config["externals"] = {
     "config": JSON.stringify({
-      "apiUrl": "http://localhost:5001/api",
-      "websocketUrl": "http://localhost:5001/chat"
+      "apiUrl": "http://localhost:5001/api"
     })
   }
   config["resolve"] = {
